df1e8514dde76ad0a5514a20beb34fa7fc684510
j-west <westja89@gmail.com>
2017-05-13 20:48:56 -0500

    remove some imports from main that are used in other files

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 'use strict'
 
-const { json } = require('body-parser')
 const express = require('express')
+const { json } = require('body-parser')
 const mongoose = require('mongoose')
 const { connect } = require('./database')
-const session = require('express-session')
-const bcrypt = require('bcrypt')
 const routes = require('./routes')
-const app = express()
+
 const PORT = process.env.PORT || 3000
+
+const app = express()
 
 app.use(json())
 app.use('/api/', routes)
-
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header("Access-Control-Allow-Methods", "GET, POST,HEAD, OPTIONS,PUT, DELETE, PATCH");
-//   next();
-// });
-
 
 connect()
   .then(() => {
