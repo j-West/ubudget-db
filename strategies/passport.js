b3851730fde16c1f6ae926d126619b44c10e6cf7
j-west <westja89@gmail.com>
2017-05-14 16:55:52 -0500

    Add local strategy

---
+++ strategies/passport.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/userModel');
+const { config } = require('../config');
+
+const localOptions = { usernameField: "email"}
+
+const localLogin = new LocalStrategy(localOptions, (email, password, done) => {
+    User.findOne({ email }, (error, user) => {
+        if (error) { return done(error) }
+        if(!user) { return done(null, false) }
+
+        user.comparePassword(password, (error, isMatch) => {
+            if (error) { return done(error) }
+            if (!isMatch) { return done(null, false) }
+
+            return done(null, user)
+        })
+    })
+})
+
+const jwtOptions = {
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    secretOrKey: config.secret
+};
+
+
+const jwtLogin = new JwtStrategy(jwtOptions, (payload, done) => {
+
+    User.findById(payload.sub, (err, user) => {
+        if (err) { return done(err, false); }
+
+        if (user) { done(null, user) }
+
+        done(null, false);
+    })
+})
+
+passport.use(jwtLogin)
+passport.use(localLogin);
