b16657cdc554b540cad0b2507a3b6350c2ad7015
j-west <westja89@gmail.com>
2017-05-13 20:53:32 -0500

    Add bcrypt to models file and add a hook to run before a new user is saved to hash password

--- models.js
+++ models.js
@@ -1,31 +1,53 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+
 
 const monthlyBudgetSchema = {
-  month: {
+  monthName: {
      type: String,
      required: true,
+  },
+  categories: {
+    type: [String],
+    required: true,
   },
   expenses: [Number]
 }
 
-const userSchema = {
+const userSchema = new Schema({
   email: {
     type: String,
     lowercase: true,
     required: true,
-    index: { unique: true },
+    unique: true
   },
   password: {
     type: String,
-    lowercase: true,
-    required: true,
+    required: true
   },
   budgets: [monthlyBudgetSchema]
-}
+})
+
+userSchema.pre('save', function(next) {
+  const user = this
+  console.log(user)
+
+  bcrypt.genSalt(15, (error, salt) => {
+    if (error) { return next(error)}
+      
+    bcrypt.hash(user.password, salt, null, (error, hash) => {
+      if (error) { return next(error)}
+
+      user.password = hash
+      next()
+    })
+  })
+})
 
 const MonthlyBudget = mongoose.model('monthly_budget', monthlyBudgetSchema)
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model('User', userSchema)
 
 module.exports = { User, MonthlyBudget }
