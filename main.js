7b41c1c9d8f5a065c20ac099df34d0a8b55d67b6
j-west <westja89@gmail.com>
2017-05-17 13:51:01 -0500

    Add cors package

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const { json } = require('body-parser')
 const mongoose = require('mongoose')
 const { connect } = require('./database')
 const router = require('./routes/router')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.use(json())
+app.use(cors())
 app.use("/api/", router)
 
 
