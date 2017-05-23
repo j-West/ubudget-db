e468ef30a8792a1633ae0167f77407438942a024
j-west <westja89@gmail.com>
2017-05-23 13:22:53 -0500

    move mongoose connect to main.js

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ const mongoose = require('mongoose')
 const { connect } = require('./database')
 const router = require('./routes/router')
 const cors = require('cors')
+const { auths } = require('./config')
+const mongoose = require('mongoose')
 
 const app = express()
+
+const MONGODB_URL = `mongodb://${auths.user}:${auths.pw}@ds139761.mlab.com:39761/ubudget`
+mongoose.Promise = Promise
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -17,7 +23,7 @@ app.use(cors())
 app.use("/api/", router)
 
 
-connect()
+mongoose.connect(MONGODB_URL)
   .then(() => {
     app.listen(app.get('port'), () => {
       console.log(`Listening on port ${PORT}`)
